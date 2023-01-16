7082eb10faa07e7538bdd9405aaf7dd49dd8a998
DarioDC95 <dario_95@live.com>
2023-01-16 18:05:08 +0100

    changed changeOption in

--- src/components/AppSelect.vue
+++ src/components/AppSelect.vue
@@ -6,7 +6,7 @@
         },
         data() {
             return {
-                change: "",
+                changeOption: "",
             }
         },
     }
@@ -14,7 +14,7 @@
 
 <template>
     <div class="card">
-        <select class="form-select p-1" name="select-type" id="select-type" v-model="change" @change="$emit('selection', change)">
+        <select class="form-select p-1" name="select-type" id="select-type" v-model="changeOption" @change="$emit('selection', changeOption)">
             <option value="" disabled selected>Select your archetype</option>
             <option v-for="(value, index) in archetype" :key="index" :value="value.archetype_name">{{ value.archetype_name }}</option>
         </select>
